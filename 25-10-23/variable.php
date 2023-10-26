7f894e705167b5caae27912b6e7a175c9573ccb6
TechNerds <technerds19sep@gmail.com>
2023-10-26 16:22:51 +0530

    first commit

---
+++ 25-10-23/variable.php
@@ -0,0 +1,8 @@
+<?php
+$a=5;
+$b=5.0;
+$type=TRUE;
+$name="priya";
+var_dump ($b);
+echo "<br>"
+?>
---
+++ 26-10-2023/comments.php
@@ -0,0 +1,17 @@
+<?php
+$a="TechNerds";
+/*
+echo("$a<br>");
+echo"Chandan";
+print"$a<br>";
+*/
+function add()
+{
+    $b =46;
+    echo $a;
+}
+// print"Technerds"."<br>".$b;
+print"Technerds"."<br>".$a;
+echo("Technerds"."<br>".$a);
+
+?>

6ea3e49d5ed8e09f71dd813b9589a818496f545e
TechNerds <technerds19sep@gmail.com>
2023-11-02 17:32:32 +0530

    php code added

---
+++ 02-11-2023/db.php
@@ -0,0 +1,15 @@
+<?php
+$sn="localhost";
+$un="root";
+$pss="";
+$dn="technerds";
+
+$conn =  mysqli_connect($sn,$un,$pss,$dn);
+// if($conn){
+//     echo "Connection sucsesful";
+// }
+// else{
+//     echo "Something went wrong";
+// }
+// mysqli_query($conn,"insert into `user` (NAME,age,gender,class) values ('Dhanush','90','male','2nd bca')");
+?>
--- 27-10-2023/form.php
+++ 27-10-2023/form.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Document</title>
@@ -9,19 +9,13 @@
 <form method="post">
     <label for="username">Enter your username: </label>
     <input type="text" name="username" placeholder="Enter your name"><br>
-    <!-- <input type="button" value="Submit"><br> -->
     <label for="">Enter your favorite color : </label>
     <label for="red">Red</label>
     <input type="checkbox" name="red">
     <label for="pink">pink</label>
     <input type="checkbox" name="pink"><br>
-    <!-- <input type="checkbox" name="cb">
-    <input type="checkbox" name="cb">
-    <input type="checkbox" name="cb"><br> -->
 
     <label for="">Enter your favorite color : </label>
-    <!-- <label for="color">Red</label> -->
-    <!-- <input type="reset" name="color"><br> -->
     <label for="color">Pink</label>
     <input type="radio" name="color" value="pink">
     <label for="color">Blue</label>
@@ -44,25 +38,59 @@
     <input type="week" name="" id=""><br>
     <input type="month" name="" id=""><br>
     <input type="range" name="" id=""><br>
-    <!-- <input type="" value="Valie" name=""> -->
     <button type="submit">Submit</button><br>
 </form>
-<?php
-// if(isset("post")){
+<php
 $a=$_POST["username"];
 $aa=$_POST["pass"];
 $aaa=$_POST["color"];
-// $aaaa=$_POST["india"];
-// $aaaaa=$_POST["el"];
-// $aaaaaa=$_POST["username"];
 echo $a."<br>";
 echo $aa."<br>";
 echo $aaa."<br>";
-// echo $aaaa;
-// echo $aaaaa;
-// echo $aaaaaa;
-// insert into 'user'('a') values('$a');
-// }
+
+?>
+</body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Input form</h1>
+
+<br><br>
+<form method="post">
+    <input type="text" name="name" placeholder="Enter your name"><br>
+    <input type="text" name="age" placeholder="Enter your age"><br>
+    <label for="gender">Select your gender:</label>
+
+    <label>Male</label><input type="radio" name="gender" value="male">
+    <label>FeMale</label><input type="radio" name="gender" value="female">
+    <label>Others</label><input type="radio" name="gender" value="others"><br>
+    <label for="class">Select your class</label>
+    <select name="class" placeholder="Select">
+        <option value="1bca">1st bca</option>
+        <option value="2bca">2nd bca</option>
+        <option value="3bca">3rd bca</option>
+    </select><br>
+    <input type="submit">
+</form>
+
+<!-- ***********PHP code********** -->
+<?php
+include "../02-11-2023/db.php";
+$name=$_POST['name'];
+$age=$_POST['age'];
+$gen=$_POST['gender'];
+$class=$_POST['class'];
+
+mysqli_query($conn,"insert into `user` (NAME,age,gender,class) values ('$name','$age','$gen','$class')");
 ?>
 </body>
 </html>
